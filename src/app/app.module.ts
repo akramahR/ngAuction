e1f236d053a19f7ff8a1ffa61f600d7832dc8588
akramahR <akramah86@gmail.com>
2023-02-28 10:52:54 +1100

    NgAuctionApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CaraouselComponent } from './caraousel/caraousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { SearchComponent } from './search/search.component';
+import { StarsComponent } from './stars/stars.component';
+import { ProductService } from './shared/product.service';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CaraouselComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    ProductDetailComponent,
+    SearchComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/caraousel/caraousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nga-caraousel',
+  templateUrl: './caraousel.component.html',
+  styleUrls: ['./caraousel.component.css']
+})
+export class CaraouselComponent {
+
+}
